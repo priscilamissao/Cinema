6beadfb72be921091ac3f5e33399ee27458bc146
VIERIA <priscila.vieria@vidanova.local>
2022-07-07 20:40:41 -0300

    Adicionando arquivo  em py

---
+++ Cinema.py
@@ -0,0 +1,23 @@
+
+lugar = input()
+print("Selecione um lugar no cinema!")
+
+vetor3 = ['C9', 'C10', 'C11', 'C12', 'C13', 'C14', 'C15']
+vetor2 = ['.....','B4', 'B5', 'B6', 'B7', 'B8','.....']
+vetor1 = ['...........','A1', 'A2', 'A3','...........']
+print(vetor3)
+print(vetor2)
+print(vetor1)
+
+lugar = input("Selecione o lugar: ")
+
+
+for vetor1 in vetor1:
+    if(vetor1 == 'A1'):
+        print("Seu acento é A1")   
+  
+
+
+
+
+
